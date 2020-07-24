8ba7ef02a4e5f43b673a7dbdb2457ecd50d2669d
Logan Pearce <loganpearce55@gmail.com>
2020-07-24 13:51:31 -0700

    rtd

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
 setup(name='lofti_gaiaDR2',
       version='1.0.0',
       description='Orbit fitting with Gaia astrometry',
@@ -9,4 +15,6 @@ setup(name='lofti_gaiaDR2',
       author_email='loganpearce1@email.arizona.edu',
       license='MIT',
       packages=['lofti_gaiaDR2'],
-      zip_safe=False)
+      zip_safe=False, 
+      install_requires=get_requires()
+          )
