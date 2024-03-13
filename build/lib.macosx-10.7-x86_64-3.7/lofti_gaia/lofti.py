corrected bug in astr/rb computation

build/lib.macosx-10.7-x86_64-3.7/lofti_gaia/lofti.py
@@ -405,16 +405,14 @@ class FitOrbit(object):
         #parameters = a,T,const,to,e,i,w,O,m1,dist
         numSamples = 10000
         parameters_init = draw_samples(numSamples, self.mtot_init, self.distance, self.ref_epoch)
-
         # Compute positions and velocities:
         if(python_fitOFTI):
-            # mas comes out of calc_OFTI:
-            X,Y,Z,Xdot,Ydot,Zdot,Xddot,Yddot,Zddot,parameters=calc_OFTI(parameters_init,self.ref_epoch,self.sep,self.pa)
+                X,Y,Z,Xdot,Ydot,Zdot,Xddot,Yddot,Zddot,parameters=calc_OFTI(parameters_init,self.ref_epoch,self.sep,self.pa)
         else:
-            returnArray = np.zeros((19,numSamples))
-            returnArray = calcOFTI_C(parameters_init,self.ref_epoch,self.sep,self.pa,returnArray.copy())
-            X,Y,Z,Xdot,Ydot,Zdot,Xddot,Yddot,Zddot = returnArray[0:9]
-            parameters = returnArray[9:]
+                returnArray = np.zeros((19,numSamples))
+                returnArray = calcOFTI_C(parameters_init,self.ref_epoch,self.sep,self.pa,returnArray.copy())
+                X,Y,Z,Xdot,Ydot,Zdot,Xddot,Yddot,Zddot = returnArray[0:9]
+                parameters = returnArray[9:]
 
         # Compute chi squared:
         if self.rv[0] != 0:
@@ -426,25 +424,8 @@ class FitOrbit(object):
         chi2 = ComputeChi2(data,model)
         if use_pm_cross_term:
             chi2 -= ( 2 * corr_coeff * (data[2][0] - model[2]) * (data[3][0] - model[3]) ) / (data[2][1] * data[3][1]) 
-        
-        if self.astrometry:
-            p = parameters.copy()
-            a,T,const,to,e,i,w,O,m1,dist = p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9]
-            chi2_astr = np.zeros(10000)
-            for j in range(self.astrometric_ra.shape[1]):
-                # for each date, compute XYZ for each 10000 trial orbit.  We can
-                # skip scale and rotate because that was accomplished in the calc_OFTI call above.
-                # X&Y are in mas coming out of calc_XYZ
-                X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,np.radians(i),np.radians(w),np.radians(O),self.astrometric_dates[j])
-                # Place astrometry into data array where: data[0][0]=ra obs, data[0][1]=ra err, etc:
-                data = np.array([self.astrometric_ra[:,j], self.astrometric_dec[:,j]])
-                # place corresponding predicited positions at that date for each trial orbit in mas:
-                # Astrometry must also be in mas:
-                model = np.array([Y1,X1])
-                # compute chi2 for trial orbits at that date and add to the total chi2 sum:
-                chi2_astr += ComputeChi2(data,model)
-            chi2 = chi2 + chi2_astr
             
+
         if self.astrometry:
             p = parameters.copy()
             a,T,const,to,e,i,w,O,m1,dist = p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9]
@@ -453,12 +434,10 @@ class FitOrbit(object):
             for j in range(self.astrometric_ra.shape[1]):
                 # for each date, compute XYZ for each 10000 trial orbit.  We can
                 # skip scale and rotate because that was accomplished in the calc_OFTI call above.
-                # X&Y are in mas coming out of calc_XYZ
-                X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,np.radians(i),np.radians(w),np.radians(O),self.astrometric_dates[j])
+                X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,self.astrometric_dates[j])
                 # Place astrometry into data array where: data[0][0]=ra obs, data[0][1]=ra err, etc:
                 data = np.array([self.astrometric_ra[:,j], self.astrometric_dec[:,j]])
-                # place corresponding predicited positions at that date for each trial orbit in mas:
-                # Astrometry must also be in mas:
+                # place corresponding predicited positions at that date for each trial orbit in arcsec:
                 model = np.array([Y1,X1])
                 # compute chi2 for trial orbits at that date and add to the total chi2 sum:
                 chi2_astr += ComputeChi2(data,model)
@@ -471,7 +450,7 @@ class FitOrbit(object):
                 # compute ecc anomaly at that date:
                 X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,self.user_rv_dates[j])
                 # compute velocities at that ecc anom:
-                Xdot,Ydot,Zdot = calc_velocities(a,T,to,e,np.radians(i),np.radians(w),np.radians(O),dist,E1)
+                Xdot,Ydot,Zdot = calc_velocities(a,T,to,e,i,w,O,dist,E1)
                 # compute chi2:
                 chi2_rv += ComputeChi2(np.array([self.user_rv[:,j]]),np.array([Zdot]))
             chi2 = chi2 + chi2_rv
@@ -532,11 +511,11 @@ class FitOrbit(object):
                 for j in range(self.astrometric_ra.shape[1]):
                     # for each date, compute XYZ for each 10000 trial orbit.  We can
                     # skip scale and rotate because that was accomplished in the calc_OFTI call above.
-                    X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,np.radians(i),np.radians(w),np.radians(O),self.astrometric_dates[j])
+                    X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,self.astrometric_dates[j])
                     # Place astrometry into data array where: data[0][0]=ra obs, data[0][1]=ra err, etc:
                     data = np.array([self.astrometric_ra[:,j], self.astrometric_dec[:,j]])
                     # place corresponding predicited positions at that date for each trial orbit:
-                    model = np.array([Y1,X1])
+                    model = np.array([Y1*1000,X1*1000])
                     # compute chi2 for trial orbits at that date and add to the total chi2 sum:
                     chi2_astr += ComputeChi2(data,model)
                 chi2 = chi2 + chi2_astr
@@ -550,7 +529,7 @@ class FitOrbit(object):
                     # compute ecc anomaly at that date:
                     X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,self.user_rv_dates[j])
                     # compute velocities at that ecc anom:
-                    Xdot,Ydot,Zdot = calc_velocities(a,T,to,e,np.radians(i),np.radians(w),np.radians(O),dist,E1)
+                    Xdot,Ydot,Zdot = calc_velocities(a,T,to,e,i,w,O,dist,E1)
                     # compute chi2:
                     chi2_rv += ComputeChi2(np.array([self.user_rv[:,j]]),np.array([Zdot]))
                 chi2 = chi2 + chi2_rv
@@ -579,7 +558,7 @@ class FitOrbit(object):
             if np.nanmin(chi2) < self.chi_min:
                 # If there is a new min chi2:
                 self.chi_min = np.nanmin(chi2)
-                #print('found new chi min:',self.chi_min)
+                print('found new chi min:',self.chi_min)
                 # re-evaluate to accept/reject with new chi_min:
                 
                 if number_orbits_accepted != 0:

build/lib.macosx-10.7-x86_64-3.7/lofti_gaia/loftitools.py
@@ -384,7 +384,7 @@ def draw_samples(number, m_tot, d_star, date):
     w = np.random.uniform(0.0,360.0,number)
     w = np.radians(w) 
     # collect into array
-    samples = np.zeros((10,number))
+    samples = np.zeros((10,10000))
     samples[0,:],samples[1,:],samples[2,:],samples[3,:],samples[4,:],samples[5,:], \
         samples[6,:],samples[7,:],samples[8,:],samples[9,:] = a,T,const,to,e,i,w,O,m1,dist
     #samples = np.array([a,T,const,to,e,i,w,O,m1,dist])
@@ -488,7 +488,7 @@ def calc_XYZ(a,T,to,e,i,w,O,date):
     X = r * ( cos(O)*cos(w+f) - sin(O)*sin(w+f)*cos(i) )
     Y = r * ( sin(O)*cos(w+f) + cos(O)*sin(w+f)*cos(i) )
     Z = r * sin(w+f)*sin(i)
-    return X*1000,Y*1000,Z*1000,E
+    return X,Y,Z,E
 
 def calc_velocities(a,T,to,e,i,w,O,dist,E):
     ''' Compute 3-d velocity of a single object on a Keplerian orbit given a 
@@ -623,14 +623,14 @@ def calc_OFTI(parameters,date,rho,pa):
     p = parameters
     # pull values out of array:
     a,T,const,to,e,i,w,O,m1,dist = p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9]
-    # Calculate predicted positions in mas at observation date:
+    # Calculate predicted positions at observation date:
     X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,date)
     # scale and rotate:
     a2,T2,to2,O2 = scale_and_rotate(X1,Y1,rho,pa,a,const,m1,dist,date)
-    # recompute predicted position in mas:
+    # recompute predicted position:
     X2,Y2,Z2,E2 = calc_XYZ(a2,T2,to2,e,i,w,O2,date)
     # convert units:
-    #X2,Y2,Z2 = (X2*u.arcsec).to(u.mas).value, (Y2*u.arcsec).to(u.mas).value, (Z2*u.arcsec).to(u.mas).value
+    X2,Y2,Z2 = (X2*u.arcsec).to(u.mas).value, (Y2*u.arcsec).to(u.mas).value, (Z2*u.arcsec).to(u.mas).value
     # Compute velocities at observation date:
     Xdot,Ydot,Zdot = calc_velocities(a2,T2,to2,e,i,w,O2,dist,E2)
     # Compute accelerations at observation date:

build/lib.macosx-10.9-x86_64-cpython-39/lofti_gaia/lofti.py
@@ -407,6 +407,7 @@ class FitOrbit(object):
         parameters_init = draw_samples(numSamples, self.mtot_init, self.distance, self.ref_epoch)
         # Compute positions and velocities:
         if(python_fitOFTI):
+                # mas comes out of calc_OFTI:
                 X,Y,Z,Xdot,Ydot,Zdot,Xddot,Yddot,Zddot,parameters=calc_OFTI(parameters_init,self.ref_epoch,self.sep,self.pa)
         else:
                 returnArray = np.zeros((19,numSamples))
@@ -434,10 +435,12 @@ class FitOrbit(object):
             for j in range(self.astrometric_ra.shape[1]):
                 # for each date, compute XYZ for each 10000 trial orbit.  We can
                 # skip scale and rotate because that was accomplished in the calc_OFTI call above.
+                # X&Y are in arcsec coming out of calc_XYZ
                 X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,self.astrometric_dates[j])
                 # Place astrometry into data array where: data[0][0]=ra obs, data[0][1]=ra err, etc:
                 data = np.array([self.astrometric_ra[:,j], self.astrometric_dec[:,j]])
                 # place corresponding predicited positions at that date for each trial orbit in arcsec:
+                # Astrometry must also be in arcsec:
                 model = np.array([Y1,X1])
                 # compute chi2 for trial orbits at that date and add to the total chi2 sum:
                 chi2_astr += ComputeChi2(data,model)
@@ -515,7 +518,7 @@ class FitOrbit(object):
                     # Place astrometry into data array where: data[0][0]=ra obs, data[0][1]=ra err, etc:
                     data = np.array([self.astrometric_ra[:,j], self.astrometric_dec[:,j]])
                     # place corresponding predicited positions at that date for each trial orbit:
-                    model = np.array([Y1*1000,X1*1000])
+                    model = np.array([Y1,X1])
                     # compute chi2 for trial orbits at that date and add to the total chi2 sum:
                     chi2_astr += ComputeChi2(data,model)
                 chi2 = chi2 + chi2_astr
@@ -558,7 +561,7 @@ class FitOrbit(object):
             if np.nanmin(chi2) < self.chi_min:
                 # If there is a new min chi2:
                 self.chi_min = np.nanmin(chi2)
-                print('found new chi min:',self.chi_min)
+                #print('found new chi min:',self.chi_min)
                 # re-evaluate to accept/reject with new chi_min:
                 
                 if number_orbits_accepted != 0:
@@ -974,6 +977,9 @@ class StatsSubclass(Stats):
 
         
         
+
+
+
 
 
 

lofti_gaia/loftitools.py
@@ -384,7 +384,7 @@ def draw_samples(number, m_tot, d_star, date):
     w = np.random.uniform(0.0,360.0,number)
     w = np.radians(w) 
     # collect into array
-    samples = np.zeros((10,10000))
+    samples = np.zeros((10,number))
     samples[0,:],samples[1,:],samples[2,:],samples[3,:],samples[4,:],samples[5,:], \
         samples[6,:],samples[7,:],samples[8,:],samples[9,:] = a,T,const,to,e,i,w,O,m1,dist
     #samples = np.array([a,T,const,to,e,i,w,O,m1,dist])
@@ -421,7 +421,7 @@ def scale_and_rotate(X,Y,rho,pa,a,const,m1,dist,d):
     import numpy as np
     
     r_model = np.sqrt((X**2)+(Y**2))
-    rho_rand = np.random.normal(rho[0]/1000.,rho[1]/1000.) #This generates a gaussian random to 
+    rho_rand = np.random.normal(rho[0],rho[1]) #This generates a gaussian random to 
     #scale to that takes observational uncertainty into account.  
 
     # scale:
@@ -471,7 +471,7 @@ def calc_XYZ(a,T,to,e,i,w,O,date):
 
     Return: 
         3-tuple:
-            X, Y, Z: 3d positions in as
+            X, Y, Z: 3d positions in mas
 
     Written by Logan Pearce, 2018
     '''
@@ -488,7 +488,7 @@ def calc_XYZ(a,T,to,e,i,w,O,date):
     X = r * ( cos(O)*cos(w+f) - sin(O)*sin(w+f)*cos(i) )
     Y = r * ( sin(O)*cos(w+f) + cos(O)*sin(w+f)*cos(i) )
     Z = r * sin(w+f)*sin(i)
-    return X,Y,Z,E
+    return X*1000,Y*1000,Z*1000,E
 
 def calc_velocities(a,T,to,e,i,w,O,dist,E):
     ''' Compute 3-d velocity of a single object on a Keplerian orbit given a 
@@ -594,15 +594,15 @@ def calc_OFTI(parameters,date,rho,pa):
 
     Args:
         Parameters (9 x N array):
-        a (flt): semi-major axis, as
-        T (flt): period, yrs
-        to (flt): epoch of periastron passage (in same time structure as dates), yrs
-        e (flt): eccentricity
-        i (flt): inclination, rad
-        w (flt): argument of periastron, rad
-        O (flt): longitude of nodes, rad
-        m1 (flt): system mass, Msun
-        dist (flt): distance to system in pc, pc
+            a (flt): semi-major axis, as
+            T (flt): period, yrs
+            to (flt): epoch of periastron passage (in same time structure as dates), yrs
+            e (flt): eccentricity
+            i (flt): inclination, rad
+            w (flt): argument of periastron, rad
+            O (flt): longitude of nodes, rad
+            m1 (flt): system mass, Msun
+            dist (flt): distance to system, pc
         date (flt): observation date, yrs
         rho (tuple, flt): separation and error, mas
         pa (tuple, flt): position angle and error, deg
@@ -615,6 +615,17 @@ def calc_OFTI(parameters,date,rho,pa):
 
             X ddot, Y ddot, Z ddot: 3d accelerations in m/s/yr
 
+            parameters: the input parameter array with the new a and O values after scale and rotate
+                    a (flt): semi-major axis, as
+                    T (flt): period, yrs
+                    to (flt): epoch of periastron passage (in same time structure as dates), yrs
+                    e (flt): eccentricity
+                    i (flt): inclination, deg
+                    w (flt): argument of periastron, deg
+                    O (flt): longitude of nodes, deg
+                    m1 (flt): system mass, Msun
+                    dist (flt): distance to system, pc
+
     Written by Logan Pearce, 2018
     '''
     import numpy as np
@@ -623,14 +634,14 @@ def calc_OFTI(parameters,date,rho,pa):
     p = parameters
     # pull values out of array:
     a,T,const,to,e,i,w,O,m1,dist = p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9]
-    # Calculate predicted positions at observation date:
+    # Calculate predicted positions in mas at observation date:
     X1,Y1,Z1,E1 = calc_XYZ(a,T,to,e,i,w,O,date)
     # scale and rotate:
     a2,T2,to2,O2 = scale_and_rotate(X1,Y1,rho,pa,a,const,m1,dist,date)
-    # recompute predicted position:
+    # recompute predicted position in mas:
     X2,Y2,Z2,E2 = calc_XYZ(a2,T2,to2,e,i,w,O2,date)
     # convert units:
-    X2,Y2,Z2 = (X2*u.arcsec).to(u.mas).value, (Y2*u.arcsec).to(u.mas).value, (Z2*u.arcsec).to(u.mas).value
+    #X2,Y2,Z2 = (X2*u.arcsec).to(u.mas).value, (Y2*u.arcsec).to(u.mas).value, (Z2*u.arcsec).to(u.mas).value
     # Compute velocities at observation date:
     Xdot,Ydot,Zdot = calc_velocities(a2,T2,to2,e,i,w,O2,dist,E2)
     # Compute accelerations at observation date: